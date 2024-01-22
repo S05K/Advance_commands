bbff3137e4e36e4530aec2878afb9cc3b9283c20
SO5K <sr966903@gmail.com>
2024-01-22 15:41:54 +0530

    fourth commit

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -2,4 +2,20 @@ class SubjectsController < ApplicationController
   def index
     @subjects = Subject.all
   end
+
+  def create
+    @subject = Subject.new(subject_params)
+    if @subject.save 
+      render json: @subject, status: 200
+    else
+      render json: "error"
+    end
+  end
+
+
+
+  private 
+  def subject_params
+    params.permit(:name, :type)
+  end
 end
